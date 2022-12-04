5487e2b026c16f9515508683a451012b75a983e1
viju-dev <71461702+viju-dev@users.noreply.github.com>
2022-12-04 12:20:04 +0530

    Add files via upload

---
+++ src/airline_reservation_system/AirLine_Reservation_System.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airline_reservation_system;
+
+/**
+ *
+ * @author viju
+ */
+public class AirLine_Reservation_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
